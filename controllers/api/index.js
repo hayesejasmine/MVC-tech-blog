cb065b2a0cbecb87cdcfe747600ed4ed4a498e8a
hayesejasmine <hayesejasmine@gmail.com>
2023-01-30 08:15:04 -0500

    views, schema, models, controllers updated..seed data created

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+//http://localhost:3001/api/users
+router.use('/users', userRoutes);
+//http://localhost:3001/api/posts
+router.use('/posts', postRoutes);
+//http://localhost:3001/api/comments
+router.use('/comments', commentRoutes);
+
+module.exports = router;
